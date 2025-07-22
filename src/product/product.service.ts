a19abde9e62028a7bc35f9240e72f0d966291e7f
Samiul Shuvo <samiulshuvo.atom@gmail.com>
2025-07-23 01:08:40 +0600

    Routing done for individual ids using service and controller

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    private products = [
+        
+            {id:1, name: "Mobile" , price: 20000},
+            {id:2, name: "Laptop" , price: 30000},
+            {id:3, name: "Tv" , price: 40000},
+    ];
+
+    getAllProducts(){
+       return this.products;
+
+    }
+    getProductById(id:number){
+        return this.products.find((product)=>
+             product.id === id
+    )
+    }
+
+    
+}
