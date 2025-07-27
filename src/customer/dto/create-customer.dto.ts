e9256474a132a0b7746e0d85f01c279ac52e1f58
Samiul Shuvo <samiulshuvo.atom@gmail.com>
2025-07-27 15:14:35 +0600

    Class Validator and Class Transformer implemented

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,11 @@
 /* eslint-disable prettier/prettier */
+import { IsInt, IsString } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateCustomerDto {
+
+    @IsString()
     name : string;
+    @IsInt()
     age : number;
 }
